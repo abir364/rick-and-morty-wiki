898c6ce1b6e0360406d51c4b568bcb856d01707f
abir364 <abirh364@gmail.com>
2023-02-13 09:41:48 +0600

    pages/SharedLayout: created shared layout page and its styling

---
+++ src/pages/SharedLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import './SharedLayout.css';
+
+const SharedLayout = () => {
+    return (
+        <div className="shared">
+            <Link to="/"><img src="src\assets\Logo.png" alt="Logo"/></Link>
+            <Outlet />
+        </div>
+    );
+}
+
+export default SharedLayout;
